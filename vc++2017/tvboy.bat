3d0fe98686e7ee73f6e61c79020747137f416fbe
Charles Doty <cdoty@users.noreply.github.com>
2026-06-05 12:44:59 -0500

    Initial commit
    Initial commit of Common Source Code Project with code updates to support Inufuto's TVBoy cartridge. and preliminary ASIEL PECOS support.

---
+++ vc++2017/tvboy.bat
@@ -0,0 +1,20 @@
+echo off
+
+if exist "%ProgramFiles(x86)%" goto is_x64
+set path="%ProgramFiles%\Microsoft Visual Studio\2017\WDExpress\MSBuild\15.0\Bin";%PATH%
+goto start
+
+:is_x64
+set path="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\WDExpress\MSBuild\15.0\Bin";%PATH%
+
+:start
+rmdir /s /q build_vc15
+mkdir build_vc15
+
+msbuild.exe tvboy.vcxproj /t:clean;rebuild /p:Configuration=Release;Platform="x64"
+mkdir build_vc15\tvboy
+copy bin\x64\Release\tvboy.exe build_vc15\tvboy\.
+
+pause
+echo on
+exit /b
